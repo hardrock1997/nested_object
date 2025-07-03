fc378dae78adeea4076ac8a3b6f8cfe3f1f1d9e4
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2025-07-03 15:31:51 +0530

    Update nested_object.js

--- nested_object.js
+++ nested_object.js
@@ -16,3 +16,53 @@ function flattenArray(arr) {
 const res1=flattenArray([1, [2, [3, 4], 5], 6]);
 const res2=flattenArray([["a"], ["b", ["c", "d"]], "e"]);
 console.log(res1, res2)
+
+
+const input = {
+  user: {
+    id: 1,
+    profile: {
+      name: "Alice",
+      contact: {
+        email: "alice@example.com",
+        phone: "12345"
+      }
+    }
+  },
+  status: "active"
+};
+
+/*
+output:
+{
+  "user.id": 1,
+  "user.profile.name": "Alice",
+  "user.profile.contact.email": "alice@example.com",
+  "user.profile.contact.phone": "12345",
+  "status": "active"
+}
+*/
+
+function flatten(input) {
+    let toReturn={}
+    const keys=Object.keys(input)
+    for(const key of keys) {
+        if(typeof input[key]==="object") {
+            const obj=flatten(input[key])
+            const ks=Object.keys(obj)
+            for(const x of ks) {
+                const ckey = `${key}.${x}`
+                toReturn[ckey]=obj[x]
+            }
+        }
+        else {
+            toReturn[key]=input[key]
+        }
+    }
+    return toReturn
+    
+}
+
+const res=flatten(input)
+console.log(res)
+
